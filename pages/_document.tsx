4c75a1bfdc4de5dcbf822400ad2f70f4e2e31ed8
zeret <kirill03192@gmail.com>
2023-06-05 17:44:30 +0300

    Fix font attribute

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ const Document: FC = () => {
         <Html>
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;600;700&family=Poppins:wght@300;400;500&display=swap"
                     rel="stylesheet"
