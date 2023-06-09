7e1ea57460b861d6b832d75797e2a3e3b3023386
zeret <kirill03192@gmail.com>
2023-06-09 17:43:48 +0300

    Fix local storage note

--- supabase/supabaseClient.ts
+++ supabase/supabaseClient.ts
@@ -1,6 +1,17 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabaseClient = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-)
+export const supabaseClient = (() => {
+    if (typeof window !== 'undefined') {
+        return createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY, {
+            auth: {
+                storage: localStorage,
+                persistSession: true,
+            },
+        })
+    }
+    return createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY, {
+        auth: {
+            persistSession: false,
+        },
+    })
+})()
