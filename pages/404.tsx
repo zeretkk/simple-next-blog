b279ac67c8f5c82d209e5b4760caf140fb8cc074
zeret <kirill03192@gmail.com>
2023-06-14 18:23:15 +0300

    Meta info for 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,20 +2,22 @@ import { Button, Grid, Typography } from '@mui/material'
 import Link from 'next/link'
 import { FC } from 'react'
 import UndoIcon from '@mui/icons-material/Undo'
+import Meta from '../components/Meta'
 
 const Error404Page: FC = () => {
     return (
         <Grid container alignItems={'center'} justifyContent={'center'} sx={{ my: '10em' }}>
+            <Meta title='Запрашиваемая страница не найдена' description='Страница остуствует по этому адресу' />
             <Grid item>
-                <Typography variant="h3" align="center" fontWeight="bold">
+                <Typography variant='h3' align='center' fontWeight='bold'>
                     404
                 </Typography>
-                <Typography variant="subtitle1" align="center">
+                <Typography variant='subtitle1' align='center'>
                     Страница не найдена
                 </Typography>
-                <Link href="/">
+                <Link href='/'>
                     <Button fullWidth>
-                        <UndoIcon /> Вернуться назад
+                        <UndoIcon /> Вернуться на главную
                     </Button>
                 </Link>
             </Grid>
